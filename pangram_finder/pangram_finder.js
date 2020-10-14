011a05cde47c0977ed671ce6e37265d9794387d4
svvishjs <andreww92@live.com>
2020-10-14 21:40:27 +0100

    completed pangram finder

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,18 @@
 const PangramFinder = function (phrase) {
-  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-}
+	this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+	this.phrase = phrase.toLowerCase();
+};
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+	const lowerPhrase = this.phrase.split('');
+	const check = this.alphabet.map((letter) => {
+		return lowerPhrase.includes(letter);
+	});
+	if (check.includes(false) === true) {
+		return false;
+	} else {
+		return true;
+	}
+};
 
 module.exports = PangramFinder;
